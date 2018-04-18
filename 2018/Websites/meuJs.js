14bdebea721f6c2dde19142bda55cb0b60daf6b7
Prof. Julio Momente <prof.juliomomente@gmail.com>
2018-04-18 19:10:51 -0300

    conteúdos de aula

---
+++ 2018/Websites/meuJs.js
@@ -0,0 +1,23 @@
+﻿$(document).ready(function () {
+    $("#titulo1").text("LTP2");
+
+    $("#btnOk").click(function () {
+        var tamanho = $("#txtCEP").val().length;
+        if (tamanho != 9) {
+            $("#txtCEP").removeClass("sucess");
+            $("#txtCEP").addClass("erro");
+            $("#txtCEP").focus();
+        } else {
+            $("#txtCEP").removeClass("erro");
+            $("#txtCEP").addClass("sucess");
+        }
+    });
+
+    $("#txtCEP").keypress(function () {
+        var tamanho = $(this).val().length;
+        //alert(tamanho);
+        if (tamanho == 5) {
+            $(this).val($(this).val() + "-");
+        }
+    });
+});
